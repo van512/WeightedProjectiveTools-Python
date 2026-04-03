d71f3cfd041a4b6e9c921176f56938505af5e9da
van512 <evannesmith31@gmail.com>
2026-04-02 22:23:19 -0500

    deleted some obsolete files

--- run_app.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import subprocess
-
-sys.path.insert(0, os.path.abspath("src"))
-os.environ["PYTHONPATH"] = "src"
-
-subprocess.run(["streamlit", "run", "streamlit_app/StreamlitApp.py"])
